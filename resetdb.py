44cab9a62173472062282e9224de51501c31be3e
PashmiDev <lowneonema@gmail.com>
2021-02-16 16:36:48 +0100

    resetdb now drop then create db

--- resetdb.py
+++ resetdb.py
@@ -3,6 +3,9 @@ import os
 """
 Standalone script used to dump the database and the images
 """
+
+db.drop_all()
+db.create_all()
 res = [r.project_name for r in db.session.query(Project).all()]
 db.session.query(Project).delete()
 db.session.query(Project_files).delete()
