5940d75adc76a72562a1042b165b7f6ba59c2924
PashmiDev <lowneonema@gmail.com>
2021-03-04 09:38:32 +0100

    fix gunicorn 404

--- app.py
+++ app.py
@@ -11,5 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+import view
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get('PORT', 80))
