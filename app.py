0f4eec8ed923f1bab28e8804df5146ea98d34c7f
PashmiDev <lowneonema@gmail.com>
2021-03-04 10:11:55 +0100

    deploy path moving to absolute

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 UPLOAD_FOLDER = '/static/upload/'
 ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
